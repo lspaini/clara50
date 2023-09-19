482eec7cc9b96f9599a70ae1ba0f5dffc520931a
Livio Spaini <lspaini@gmail.com>
2023-09-19 12:05:16 +0200

    new async zeugs

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Full from '../images/full_high.png';
 import { LandingContainer, LandingImageContainer, TitleContainer, Title } from '../styles/LandingStyles';
 
+const isDevelopment = process.env.NODE_ENV === "development";
+
 /**
  * Returns the aspect ratio of an image given its source URL.
- * @param src - The source URL of the image.
- * @returns A promise that resolves with the aspect ratio of the image.
  */
 const getImageAspectRatio = (src: string): Promise<number> => {
   return new Promise((resolve, reject) => {
@@ -22,89 +22,126 @@ const getImageAspectRatio = (src: string): Promise<number> => {
 // get aspect ratio of the viewport (window), even when resized, and put it in the console.
 window.addEventListener('resize', () => {
   const aspectRatio = window.innerWidth / window.innerHeight;
-  console.log("Viewport AR: ", aspectRatio);
+  if (isDevelopment) console.log("Viewport AR: ", aspectRatio);
+});
+// get aspect ratio of the viewport (window), even when resized, and put it in the console.
+window.addEventListener('scroll', () => {
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  if (isDevelopment) console.log("Viewport AR: ", aspectRatio);
 });
 
 
 
-/**
- * Landing component that displays a background image that adjusts its size and position based on scrolling and window dimensions.
- * @returns React functional component
- */
 const Landing: React.FC = () => {
   
-  // Zoom Options
-  const initialBgSize = 250;
-  const startResizeScrollPercent = 20;
-  const endResizeScrollPercent = 60;
+  // Initial background size (in %) when the page loads
+  const initialImageSize = 250;
+
+   // Define boundaries for zooming (in %) and shrinking the image
+   const startResizeScrollPercent = 20;
+   const endResizeScrollPercent = 60;
 
   const [minHeight, setMinHeight] = useState<number | null>(null);
   const [minWidth, setMinWidth] = useState<number | null>(null);
   
-  const [bgSize, setBgSize] = useState(`${initialBgSize}%`);
-  const [bgPosY, setBgPosY] = useState('100%');
+  const [imageSize, setImageSize] = useState(`${initialImageSize}%`);
+  const [imagePosY, setImagePosY] = useState('100%');
 
-  const [minBgSize, setMinBgSize] = useState(100);
+  const [minImageSize, setMinImageSize] = useState(100);
   
 
+const adjustImageSize = (percentage: number): number => {
+
+  let newSize;
+
+  // If the scroll percentage is less than the start boundary, gradually zoom out the image
+  if (percentage < startResizeScrollPercent) {
+    newSize = initialImageSize - (percentage / startResizeScrollPercent) * (initialImageSize - minImageSize);
+    if (isDevelopment) console.log("percentage is less than the start boundary");
+    return Math.max(newSize, minImageSize);
+  } 
+
+  // If the scroll percentage is between the start and end boundaries, keep the image at its smallest size
+  else if (percentage >= startResizeScrollPercent && percentage < endResizeScrollPercent) {
+    if (isDevelopment) console.log("percentage is between the start and end boundaries")
+    return minImageSize;
+  } 
+
+  // If the scroll percentage is greater than the end boundary, gradually zoom in the image
+  else {
+    newSize = minImageSize + ((percentage - endResizeScrollPercent) / (100 - endResizeScrollPercent)) * (initialImageSize - minImageSize);
+    if (isDevelopment) console.log("percentage is greater than the end boundary");
+    return Math.min(newSize, initialImageSize);
+  }
+};
+
+  
+/**
+ * Adjusts the background Y-position based on the scroll percentage.
+ * 
+ * This function modifies the background Y-position to create a scroll effect.
+ * The Y-position starts from 100 (fully visible) and decreases to 0 (top of the image at the viewport's top edge)
+ * as the user scrolls down the page.
+ */
+const adjustImagePosY = (percentage: number): number => {
+  // Calculate new Y-position as a percentage
+  // It starts from 100% (fully visible) and goes down to 0% (aligned with the top of the viewport)
+  const newYPosition = 100 - (percentage / 100) * 100;
+  // console.log("newYPosition: ", newYPosition);
+  // Ensure that the new Y-position does not go below 0%
+  return Math.max(newYPosition, 0);
+};
 
   /**
    * Handles the scroll event and updates the background size and position based on the scroll percentage.
    */
   const handleScroll = () => {
+
+  
     // Calculate Scroll Height and Scroll Percentage
     const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
     const scrollPercentage = (window.scrollY / scrollHeight) * 100;
   
-    // Helper function to adjust background size
-    const adjustBgSize = (percentage:number) => {
-      let newSize;
-      if (percentage < startResizeScrollPercent) {
-        newSize = initialBgSize - (percentage / startResizeScrollPercent) * (initialBgSize - minBgSize);
-        return Math.max(newSize, minBgSize);
-      } else if (percentage >= startResizeScrollPercent && percentage < endResizeScrollPercent) {
-        return minBgSize;
-      } else {
-        newSize = minBgSize + ((percentage - endResizeScrollPercent) / (100 - endResizeScrollPercent)) * (initialBgSize - minBgSize);
-        return Math.min(newSize, initialBgSize);
-      }
-    };
-  
-    // Helper function to adjust background position Y
-    const adjustBgPosY = (percentage:number) => {
-      const newY = 100 - (percentage / 100) * 100;
-      return Math.max(newY, 0);
-    };
-  
     // Set background size and position
-    setBgSize(`${adjustBgSize(scrollPercentage)}%`);
-    setBgPosY(`${adjustBgPosY(scrollPercentage)}%`);
+    setImageSize(`${adjustImageSize(scrollPercentage)}%`);
+    setImagePosY(`${adjustImagePosY(scrollPercentage)}%`);
+
+    if (isDevelopment) console.log("Updated imageSize: ", imageSize); // Log the updated size
   };
 
   useEffect(() => {
-    // Check if running in browser environment
-    if (typeof window !== 'undefined') {
-
-      const aspectRatioWindow = window.innerWidth / window.innerHeight;
-      // Set initial dimensions
-      setMinHeight(window.innerHeight);
-      setMinWidth(window.innerWidth);
+    const init = async () => {
+      try {
+        // Check if running in browser environment
+        if (typeof window !== 'undefined') {
+          
+          // Get the aspect ratio of the window
+          const aspectRatioWindow = window.innerWidth / window.innerHeight;
+    
+          // Set initial dimensions
+          setMinHeight(window.innerHeight);
+          setMinWidth(window.innerWidth);
+    
+          // Fetch the image's aspect ratio
+          const aspectRatioImage = await getImageAspectRatio(Full);
+    
+          if (isDevelopment) console.log("Image AR: ", aspectRatioImage);
+    
+          // Calculate the minimum zoom level needed for the "cover" effect
+          const minCoverSize = aspectRatioWindow > aspectRatioImage
+            ? (window.innerHeight / window.innerWidth) * aspectRatioImage * 350
+            : (window.innerWidth / window.innerHeight) / aspectRatioImage * 150;
   
-      getImageAspectRatio(Full)
-      .then((aspectRatioImage) => {
-        console.log("Image Aspect Ratio: ", aspectRatioImage);
-        
-        // Calculate minimum zoom level needed for "cover" effect.
-        const minCoverSize = aspectRatioWindow > aspectRatioImage
-          ? (window.innerHeight / window.innerWidth) * aspectRatioImage * 350
-          : (window.innerWidth / window.innerHeight) / aspectRatioImage * 250;
-
-        // Set this size as your new minimum size
-        setMinBgSize(minCoverSize);
-      })
-      .catch((error) => {
+          console.log("minCoverSize: ", minCoverSize);
+          
+          // Update the state with the new minimum background size
+          setMinImageSize(minCoverSize);
+        }
+      } catch (error) {
+        // Log any errors encountered while fetching the image aspect ratio
         console.error("Error getting image: ", error);
-      });
+      }
+  
       // Your existing event listeners
       window.addEventListener('scroll', handleScroll);
   
@@ -112,15 +149,20 @@ const Landing: React.FC = () => {
       return () => {
         window.removeEventListener('scroll', handleScroll);
       };
-    }
-  }, [bgSize, minHeight, minWidth]);
+    };
+  
+    // Call the inner async function
+    init();
+    
+  }, [imageSize, minHeight, minWidth]);
+  
   
   
   
 
   return (
     <LandingContainer>
-      <LandingImageContainer image={Full} style={{ backgroundSize: bgSize, backgroundPositionY: bgPosY }} />
+      <LandingImageContainer image={Full} style={{ backgroundSize: imageSize, backgroundPositionY: imagePosY }} />
           <TitleContainer>
             <Title bottom={0}>Clarastrasse 50</Title>
           </TitleContainer>
